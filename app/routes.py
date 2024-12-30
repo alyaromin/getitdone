e92d6a6bd6d5f9d41d2d059c16f44bab0433a560
alyaromin <alyaromin@gmail.com>
2024-12-30 18:17:32 +0300

    feat: added connection to db

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from flask import render_template, redirect, url_for, request, flash
-from app import app
+from app import app, db
+from sqlalchemy import text
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/check_connection')
+def check_connection():
+    try:
+        # Пытаемся выполнить простой запрос к базе данных
+        db.session.execute(text("SELECT 1"))
+        return "Подключение к базе данных успешно!"
+    except Exception as e:
+        return f"Ошибка подключения: {e}"
